bca72c5d01d3269b4405180fd35c9c3054185626
Hamzaoui zied <hamzaoui.zied.93@gmail.com>
2024-05-07 19:14:04 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,46 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<!--
+=========================================================
+* Vue Argon Dashboard 2 PRO - v4.0.0
+=========================================================
+
+* Product Page: https://creative-tim.com/product/vue-argon-dashboard-pro
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+import Sidenav from "./examples/Sidenav";
+import Configurator from "@/examples/Configurator.vue";
+import Navbar from "@/examples/Navbars/Navbar.vue";
+import AppFooter from "@/examples/Footer.vue";
+
+const store = useStore();
+const layout = computed(() => store.state.layout);
+const showSidenav = computed(() => store.state.showSidenav);
+const showNavbar = computed(() => store.state.showNavbar);
+const showFooter = computed(() => store.state.showFooter);
+const showConfig = computed(() => store.state.showConfig);
+const hideConfigButton = computed(() => store.state.hideConfigButton);
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <div
+    v-show="layout === 'landing'"
+    class="landing-bg h-100 bg-gradient-primary position-fixed w-100"
+  ></div>
+  <sidenav v-if="showSidenav" />
+  <main class="main-content position-relative max-height-vh-100 h-100">
+    <navbar v-if="showNavbar" />
+    <router-view />
+    <app-footer v-show="showFooter" />
+    <configurator
+      :class="[showConfig ? 'show' : '', hideConfigButton ? 'd-none' : '']"
+    />
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
