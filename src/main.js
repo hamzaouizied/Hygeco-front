bf6f2f6138c7f2dbf31fec0d2ca66dd8e670c338
Hamzaoui Zied <72856566+hamzaouizied@users.noreply.github.com>
2024-06-01 14:09:28 +0100

    Ticket/contact (#13)
    * wip
    
    * wip

--- src/main.js
+++ src/main.js
@@ -23,6 +23,7 @@ import VueTilt from "vue-tilt.js";
 import VueSweetalert2 from "vue-sweetalert2";
 import ArgonDashboard from "./argon-dashboard";
 
+
 const appInstance = createApp(App);
 appInstance.use(store);
 appInstance.use(router);
